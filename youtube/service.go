05f6d89741936eddc0f30aea269ce6c2493b36f3
rishusahu <rishusahu23@gmail.com>
2025-01-22 02:11:24 +0530

    integrate search api

--- youtube/service.go
+++ youtube/service.go
@@ -2,7 +2,9 @@ package youtube
 
 import (
 	"context"
+	"fmt"
 	"github.com/rishusahu23/fam-youtube/external/youtube"
+	vgPb "github.com/rishusahu23/fam-youtube/gen/api/external/youtube"
 	"github.com/rishusahu23/fam-youtube/gen/api/rpc"
 	youtubePb "github.com/rishusahu23/fam-youtube/gen/api/youtube"
 )
@@ -21,6 +23,11 @@ func NewService(googleClient youtube.Client) *Service {
 var _ youtubePb.YoutubeServiceServer = (*Service)(nil)
 
 func (s *Service) TriggerJob(ctx context.Context, request *youtubePb.TriggerJobRequest) (*youtubePb.TriggerJobResponse, error) {
+	resp, err := s.googleClient.FetchYoutubeData(ctx, &vgPb.FetchYoutubeDataListRequest{})
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(resp)
 	return &youtubePb.TriggerJobResponse{
 		Status: rpc.StatusOk(),
 	}, nil
